0e92befe3531e8cf011eae2b9d12d5c66184a6f0
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-10-27 17:58:24 +0300

    Create Camera movement script

---
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+	[SerializeField]
+	private Camera _camera;
+
+	[SerializeField] 
+	private float _speed;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_camera.transform.position += new Vector3(0, _speed * Time.deltaTime);
+	}
+}
