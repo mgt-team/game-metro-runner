8fad7416d4fe564e69cb53a1969a03e7d1cc165a
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-10-27 16:33:26 +0300

    Create PlayerManager

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private MoveController _moveController;
+    private Player _player;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlayerMovement();
+    }
+
+    private void UpdatePlayerMovement()
+    {
+        _player.SetVelocity(_moveController.GetVelocity());
+    }
+}
