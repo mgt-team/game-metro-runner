8a76b9d80f9bfa64b89592830e1702f1bf3a410c
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-16 16:32:34 +0300

    Refactor ZoneGrid

--- Assets/Scripts/Zone/ZoneGrid.cs
+++ Assets/Scripts/Zone/ZoneGrid.cs
@@ -2,58 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZoneGrid : MonoBehaviour {
+public class ZoneGrid
+{
+    private readonly int _rowCount;
+    private readonly int _columnCount;
 
-    public float xMax;
-    public float xMin;
+    private readonly Vector2 _minCoordinate;
+    private readonly Vector2 _maxCoordinate;
+    
+    private readonly float _rowDistanceBetweenPoints;
+    private readonly float _columnDistanceBetweenPoints;
 
-    public float yMax;
-    public float yMin;
+    private readonly List<List<ZonePoint>> _zonePoints;
 
-    private List<List<ZonePoint>> _zonePoints;
-
-    public ZoneGrid (Sprite background, Vector2 zonePosition, int horizontalPointCount, int verticalPointCount)
+    public ZoneGrid (Sprite background, Vector2 zonePosition, int rowCount, int columnCount)
     {
-        xMax = background.rect.xMax / 60;
-        xMin = background.rect.xMin / 60;
+        _rowCount = rowCount;
+        _columnCount = columnCount;
+        
+        _minCoordinate = new Vector2(background.rect.xMin / 60, background.rect.yMin / 60);
+        _maxCoordinate = new Vector2(background.rect.xMax / 60, background.rect.yMax / 60);
 
-        yMax = background.rect.yMax / 60;
-        yMin = background.rect.yMin / 60;
+        _rowDistanceBetweenPoints = (_maxCoordinate.x - _minCoordinate.x) / rowCount;
+        _columnDistanceBetweenPoints = (_maxCoordinate.y - _minCoordinate.y) / columnCount;
 
-        _zonePoints = GetGrid(zonePosition, horizontalPointCount, verticalPointCount);
+        _zonePoints = GetGrid(zonePosition, rowCount, columnCount);
     }
 
-    public List<List<ZonePoint>> GetGrid(Vector2 zonePosition, int horizontalPointCount, int verticalPointCount)
+    public List<List<ZonePoint>> ZonePoints
     {
-        List<List<ZonePoint>> countVZP = new List<List<ZonePoint>>();               //List of List verticalZonePoints
-        float x = (xMax - xMin) / horizontalPointCount;
-        float y = (yMax - yMin) / verticalPointCount;
-        for (int i = 1; i < verticalPointCount; i++)
+        get { return _zonePoints; }
+    }
+
+    public int RowCount
+    {
+        get { return _rowCount; }
+    }
+
+    public int ColumnCount
+    {
+        get { return _columnCount; }
+    }
+
+    private List<List<ZonePoint>> GetGrid(Vector2 zonePosition, int rowCount, int columnCount)
+    {
+        List<List<ZonePoint>> zonePoints = new List<List<ZonePoint>>();               //List of List verticalZonePoints
+        
+        for (int i = 1; i < columnCount; i++)
         {
-            List<ZonePoint> verticalZonePoints = new List<ZonePoint>();
-            for (int j = 1; j < horizontalPointCount; j++)
-                verticalZonePoints.Add(new ZonePoint(new Vector2(x * j + (zonePosition.x - xMax / 2), y * i + (zonePosition.y - yMax / 2))));
-            countVZP.Add(verticalZonePoints);
+            List<ZonePoint> currentRowPoints = new List<ZonePoint>();
+            for (int j = 1; j < rowCount; j++)
+                currentRowPoints.Add(new ZonePoint(
+                    new Vector2(_rowDistanceBetweenPoints * j + (zonePosition.x - _maxCoordinate.x / 2),
+                        _columnDistanceBetweenPoints * i + (zonePosition.y - _maxCoordinate.y / 2))));
+            zonePoints.Add(currentRowPoints);
         }
-        return countVZP;
+        return zonePoints;
 
     }
 
-    public void GetVisualGrid(Vector2 backgroundPos, int horizontalPointCount, int verticalPointCount)
+    /*
+     * Visual grid in editor
+     */ 
+    public void GetVisualGrid(Vector2 backgroundPos, int rowCount, int columnCount)
     {
-        float x = (xMax - xMin) / horizontalPointCount;
-        float y = (yMax - yMin) / verticalPointCount;
-        for(int i = 1; i < horizontalPointCount; i++)
+        var offsetVector = new Vector2(-_maxCoordinate.x / 2 + backgroundPos.x,
+            -_maxCoordinate.y / 2 + backgroundPos.y);
+        for(var i = 1; i < rowCount; i++)
         {
-            Vector2 end = new Vector2 (i * x - xMax/2 + backgroundPos.x, yMin - yMax / 2  + backgroundPos.y);                  //Этот метод строит сетку в редакторе
-            Vector2 start = new Vector2(i * x - xMax/2 + backgroundPos.x, yMax - yMax / 2 + backgroundPos.y);
+            var xCoordinate = i * _rowDistanceBetweenPoints + offsetVector.x;
+            var end = new Vector2 (xCoordinate, _minCoordinate.y + offsetVector.y);
+            var start = new Vector2(xCoordinate, _maxCoordinate.y + offsetVector.y);
             Debug.DrawLine(start, end, Color.yellow, .5f);
         }
 
-        for(int i = 1; i < verticalPointCount; i++)
+        for(var i = 1; i < columnCount; i++)
         {
-            Vector2 end = new Vector2(xMin - xMax / 2 + backgroundPos.x, y * i - yMax / 2 + backgroundPos.y);
-            Vector2 start = new Vector2(xMax - xMax / 2 + backgroundPos.x, y * i - yMax / 2 + backgroundPos.y);
+            var yCoordinate = _columnDistanceBetweenPoints * i + offsetVector.y;
+            var end = new Vector2(_minCoordinate.x + offsetVector.x, yCoordinate);
+            var start = new Vector2(_maxCoordinate.x + offsetVector.x, yCoordinate);
             Debug.DrawLine(start, end, Color.yellow, .5f);
         }
     }
